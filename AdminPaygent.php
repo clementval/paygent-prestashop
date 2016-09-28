126033b073878da40f01bf1ed45ea4cdac986b62
Valentin Clement <clementval@gmail.com>
2016-09-28 10:19:51 +0200

    Push skeleton for admin tab

---
+++ AdminPaygent.php
@@ -0,0 +1,20 @@
+<?php
+class AdminPaygent extends AdminTab {
+	public function __construct(){
+	 	$this->table = 'paygent';
+	 	$this->className = 'Paygent';
+		$this->lang = true;
+		$this->edit = true;
+		$this->delete = true;
+    
+		parent::__construct();
+	}
+
+	public function displayForm($isMainTab = true) {
+		global $currentIndex, $cookie;
+		parent::displayForm();
+		if (!($obj = $this->loadObject(true)))
+			return;
+	}
+}
+?>
