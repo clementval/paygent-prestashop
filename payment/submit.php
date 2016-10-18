3b73169abad6721d425cad3b7d69d68b302ef92a
Valentin Clement <clementval@gmail.com>
2016-10-18 16:34:15 +0200

    Push payment folder

---
+++ payment/submit.php
@@ -0,0 +1,8 @@
+<?php
+
+include_once(dirname(__FILE__).'/../../../config/config.inc.php');
+include_once(dirname(__FILE__).'/../../../init.php');
+
+include_once(_PS_MODULE_DIR_.'paygent/classes/paygent.php');
+
+echo 'Redirecting ...';
