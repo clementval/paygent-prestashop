6cc3ab791ec3b8e43109bf1c9ae7cc4cf70af3a0
Valentin Clement <clementval@gmail.com>
2016-10-18 16:25:46 +0200

    Add translation files

---
+++ fr.php
@@ -0,0 +1,12 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{paygent}prestashop>confirmation_2e2117b7c81aa9ea6931641ea2c6499f'] = 'Votre commande avec';
+$_MODULE['<{paygent}prestashop>confirmation_75fbf512d744977d62599cc3f0ae2bb4'] = 'est complet.';
+$_MODULE['<{paygent}prestashop>confirmation_4789eab88a789fb90945b22afb6b676d'] = 'Vous avez choisi la méthode Paygent.';
+$_MODULE['<{paygent}prestashop>confirmation_e6dc7945b557a1cd949bea92dd58963e'] = 'Votre commande sera préparée dans les plus bref délais.';
+$_MODULE['<{paygent}prestashop>confirmation_0db71da7150c27142eef9d22b843b4a9'] = 'Pour toutes questions or informations, veuillez contacter';
+$_MODULE['<{paygent}prestashop>confirmation_64430ad2835be8ad60c59e7d44e4b0b1'] = 'service client';
+$_MODULE['<{paygent}prestashop>paygent_7e8b5e4a76b957ee3863aaba0f3d79b3'] = 'Paygent';
+$_MODULE['<{paygent}prestashop>paygent_73eea39e6daed70bcc630eb67aa9cd9f'] = 'Paiement par carte de crédit avec Paygent.';
