eb5b43e10e6fc6cdcb4b3a4ecc12d249adb9f868
Valentin Clement <clementval@gmail.com>
2016-10-20 09:49:16 +0200

    Add form information

--- AdminPaygent.php
+++ AdminPaygent.php
@@ -13,20 +13,24 @@ class AdminPaygent extends AdminTab {
 
   public function display()
   {
-    $sql = 'SELECT * FROM '._DB_PREFIX_.'paygent_config';
-    if ($results = Db::getInstance()->ExecuteS($sql)){
+    $token = Tools::getAdminTokenLite('AdminPaygent');
+    $currentIndex = 'index.php?tab=AdminPaygent&token='.$token;
+
+    if (Tools::isSubmit('paygent_config')){
+      echo 'Saved';
+    } else {
       $merchant_id = Configuration::get('PAYGENT_MERCHANT_ID');
       $hash_key = Configuration::get('PAYGENT_HASHKEY');
       $inform_url = "";
       echo "<h2>Paygent payement configuration</h2>";
-      echo "<form>";
+
+//      <form action="' . $currentIndex . '&submitAdd' .  $this->table . '=1&token=' . $this->token . '" method="post" >
+      echo '<form name="paygent_config" action="'.$currentIndex.'" method="post">';
       echo "Merchant ID: <input type='text' name='merchant_id' value='".$merchant_id."'/></br>";
       echo "Hash key: <input type='text' name='hash_key' value='".$hash_key."'/></br>";
       echo "Inform URL: <input type='text' name='inform_url' value='".$inform_url."'/></br>";
       echo "<input type='submit' value='Save'/>";
       echo "</form>";
-    } else {
-      echo "<h2>Error: Cannot retrieve Paygent configuration from DB.</h2>";
     }
   }
 }
