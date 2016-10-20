edc7fbbb58a3ae17730c9a702370931038ece2ee
Valentin Clement <clementval@gmail.com>
2016-10-20 09:49:26 +0200

    Formatting

--- controllers/admin/AdminPaygentController.php
+++ controllers/admin/AdminPaygentController.php
@@ -5,7 +5,7 @@ class AdminPaygentController extends ModuleAdminController
   public function __construct()
   {
     $token = Tools::getAdminTokenLite('AdminModules');
-    $currentIndex='index.php?controller=AdminModules&token='.$token.'&configure=paygent&tab_module=front_office_features&module_name=paygent';
+    $currentIndex = 'index.php?controller=AdminModules&token='.$token.'&configure=paygent&tab_module=front_office_features&module_name=paygent';
     parent::__construct();
     Tools::redirectAdmin($currentIndex);
   }
