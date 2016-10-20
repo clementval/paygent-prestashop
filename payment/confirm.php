9372e073341aa3c85fa8c1d0bb62b1e799c2f64c
Valentin Clement <clementval@gmail.com>
2016-10-20 15:47:07 +0200

    Push dummy confirm page

---
+++ payment/confirm.php
@@ -0,0 +1,13 @@
+<?php
+
+  echo 'Confirmation';
+
+/*
+ information in POST
+ result=0 0 = SUCCESS 1 = FAILURE
+ response_code=
+ response_detail= url=https://service.paygent.co.jp/linktype/top?type=&tid=book_101121&mid=10011212&hv=MFEwMTAyMDAw trading_id=book_101121
+ payment_type=01,02,05 limit_date=20070707235959 trade_generation_date=20070705121030
+*/
+
+?>
