0bd88f80c9cc85e9e7b403c7ef7606671eb85370
Valentin Clement <clementval@gmail.com>
2016-11-03 12:19:48 +0100

    Update the transaction with response information

--- payment/confirm.php
+++ payment/confirm.php
@@ -1,16 +1,28 @@
 <?php
+include_once(dirname(__FILE__).'/../../../config/config.inc.php');
+include_once(dirname(__FILE__).'/../../../init.php');
+include_once(_PS_MODULE_DIR_.'paygent/classes/paygent.php');
 
-  include_once(dirname(__FILE__).'/../../../config/config.inc.php');
-  include_once(dirname(__FILE__).'/../../../init.php');
-
-  echo 'Confirmation';
+$paygent = new Paygent();
+$trading_id = trim($_POST["trading_id"]);
+$response = trim($_POST["response"]);
+$paygent->update_transaction($trading_id, $response));
 
 /*
+  $all_posts = "";
+  foreach ($_POST as $key => $value) {
+    $all_posts = $all_posts.'Field:'.htmlspecialchars($key).'//'..htmlspecialchars($value);
+  }
+
  information in POST
  result=0 0 = SUCCESS 1 = FAILURE
  response_code=
- response_detail= url=https://service.paygent.co.jp/linktype/top?type=&tid=book_101121&mid=10011212&hv=MFEwMTAyMDAw trading_id=book_101121
- payment_type=01,02,05 limit_date=20070707235959 trade_generation_date=20070705121030
+ response_detail=
+ url=https://service.paygent.co.jp/linktype/top?type=&tid=book_101121&mid=10011212&hv=MFEwMTAyMDAw
+ trading_id=book_101121
+ payment_type=01,02,05
+ limit_date=20070707235959
+ trade_generation_date=20070705121030
 */
 
 ?>
