345d4f03d6a3e66ffa23b243939304988d25da0a
Valentin Clement <clementval@gmail.com>
2016-11-03 10:24:58 +0100

    Add test page

--- AdminPaygent.php
+++ AdminPaygent.php
@@ -1,4 +1,5 @@
 <?php
+
 class AdminPaygent extends AdminTab {
 
   public function __construct()
@@ -27,6 +28,16 @@ class AdminPaygent extends AdminTab {
     echo 'Hash key: <input type="text" name="hash_key" value="'.$hash_key.'"/></br>';
     echo '<input type="submit" name="paygent_config" value="Save" />';
     echo '</form>';
+
+    echo '<br><br><br>';
+    echo '<h3>Test payment</h3>';
+    echo '<form method="POST" action="'.__PS_BASE_URI__.'modules/paygent/payment/submit_test.php" id="paygent_form">';
+    echo 'Customer ID: <input type="text" name="test_customer_id"/><br>';
+    echo 'Amount: <input type="text" name="test_amount"/><br>';
+    echo 'Trading ID: <input type="text" name="test_trading_id"/><br>';
+    echo '<input type="submit" value="Test"/>';
+    echo '</form>';
+    echo '<br><br><br>';
   }
 
   public function postProcess()
---
+++ payment/submit_test.php
@@ -0,0 +1,56 @@
+<?php
+
+include_once(dirname(__FILE__).'/../../../config/config.inc.php');
+include_once(dirname(__FILE__).'/../../../init.php');
+
+include_once(_PS_MODULE_DIR_.'paygent/classes/paygent.php');
+
+global $smarty;
+global $cart;
+$return_url = $smarty->tpl_vars['base_dir'].'history.php';
+$paygent = new Paygent();
+$paygent->load_configuration();
+$paygent_action = Configuration::get('PAYGENT_ACTION_URL');
+$merchant_id = $paygent->get_merchant_id();
+$trading_id = $_POST["test_trading_id"];
+$id = $_POST["test_amount"];
+$customer_id = $_POST["test_customer_id"];
+$payment_class = "0";
+$payment_type = "02";
+$use_card_conf_number = "1";
+$paygent->set_trading_id($trading_id);
+$paygent->set_payment_type($payment_type);
+$paygent->set_id($id);
+$paygent->set_customer_id($customer_id);
+$paygent->set_payment_class($payment_class);
+$paygent->set_use_card_conf_number($use_card_conf_number);
+$hash = $paygent->generate_hash();
+
+?>
+<html>
+  <head>
+    <title>Paygent</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  </head>
+  <body>
+    <p>
+        Test information:
+        Trading ID: <?= $trading_id ?>
+        Amount: <?= $amount ?>
+        Customer ID: <?= $customer_id ?>
+    </p>
+  <body>
+    <form method="POST" action="<?= $paygent_action ?>" class="hiddent" id="paygent_form">
+      <input type="hidden" name="trading_id" value="<?= $trading_id ?>" /> <!-- transaction id -->
+      <input type="hidden" name="payment_type" value="<?= $payment_type ?>" />
+      <input type="hidden" name="id" value="<?= $id ?>" /> <!-- amount -->
+      <input type="hidden" name="seq_merchant_id" value="<?= $merchant_id ?>" />
+      <input type="hidden" name="payment_class" value="<?= $payment_class ?>" />
+      <input type="hidden" name="use_card_conf_number" value="<?= $use_card_conf_number ?>" />
+      <input type="hidden" name="customer_id" value="<?= $customer_id ?>" />
+      <input type="hidden" name="return_url" value="<?= $return_url ?>" />
+      <input type="hidden" name="hc" value="<?= $hash ?>" />
+      <input type="submit" value="Pay"/> <!-- to be removed after dev -->
+    </form>
+  </body>
+</html>
